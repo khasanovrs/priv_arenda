f12f08f9edeabebdd4ef1e897403f4f85a4ead7d
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-06-18 16:47:45 +0300

    Чиним на сервере

--- config/web.php
+++ config/web.php
@@ -16,6 +16,7 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'd3N9QmpU1fGqAG3DfnwSMR1LnZlXD23B',
             'enableCsrfValidation' => false,
+            'baseUrl' => '',
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
             ]
