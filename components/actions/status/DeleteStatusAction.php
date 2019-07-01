b9766ce93bafcdf2191df0cb4ed907d5ce71490d
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-07-01 09:45:48 +0300

    Добавление, удаление статусов клиента

---
+++ components/actions/status/DeleteStatusAction.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Удаление статуса для клиента
+ */
+
+namespace app\components\actions\statusUr;
+
+use app\components\Status\StatusClass;
+use Yii;
+use yii\base\Action;
+
+class DeleteStatusAction extends Action
+{
+    public function run()
+    {
+        Yii::info('Запуск функции удаления статуса для клиента', __METHOD__);
+
+        $request = Yii::$app->request;
+
+        $id = $request->getBodyParam('id');
+
+        $resultChange = StatusClass::DeleteStatus($id);
+
+        if (!is_array($resultChange) || !isset($resultChange['status']) || $resultChange['status'] != 'SUCCESS') {
+            Yii::error('Ошибка при удалении статуса для клиента', __METHOD__);
+
+            if (is_array($resultChange) && isset($resultChange['status']) && $resultChange['status'] === 'ERROR') {
+                return $resultChange;
+            }
+
+            return [
+                'status' => 'ERROR',
+                'msg' => 'Ошибка при удалении статуса для клиента',
+            ];
+        }
+
+        Yii::info('Статус успешно удален', __METHOD__);
+
+        return [
+            'status' => 'ОК',
+            'msg' => 'Статус успешно удален',
+        ];
+    }
+}
--- components/status/StatusClass.php
+++ components/status/StatusClass.php
@@ -49,4 +49,87 @@ class StatusClass
             'data' => $result
         ];
     }
+
+    /**
+     * Добавление нового статуса для клиента
+     * @param $name ,
+     * @return bool|array
+     */
+    public static function AddStatus($name)
+    {
+        Yii::info('Запуск функции добавления нового статуса для юр. лиц', __METHOD__);
+
+        if ($name === '') {
+            Yii::error('Ни передано наименование статуса, name:' . serialize($name), __METHOD__);
+
+            return [
+                'status' => 'ERROR',
+                'msg' => 'Ни передано наименование статуса',
+            ];
+        }
+
+        $new_status = new ClientStatus();
+        $new_status->name = $name;
+
+        try {
+            if (!$new_status->save(false)) {
+                Yii::error('Ошибка при добавлении нового статуса для клиента: ' . serialize($new_status->getErrors()), __METHOD__);
+                return false;
+            }
+        } catch (\Exception $e) {
+            Yii::error('Поймали Exception при добавлении нового статуса для клиента: ' . serialize($e->getMessage()), __METHOD__);
+            return false;
+        }
+
+        Yii::info('Статус для клиента успешно добавлен', __METHOD__);
+
+        return [
+            'status' => 'SUCCESS',
+            'msg' => 'Статус для клиента успешно добавлен'
+        ];
+    }
+
+    /**
+     * Удаление статуса юр. лиц
+     * @param $id ,
+     * @return bool|array
+     */
+    public static function DeleteStatus($id)
+    {
+        Yii::info('Запуск функции удаления статуса для клиента', __METHOD__);
+
+        if ($id === '') {
+            Yii::error('Ни передано идентификатор статуса, id:' . serialize($id), __METHOD__);
+
+            return [
+                'status' => 'ERROR',
+                'msg' => 'Ни передан идентификатор статуса',
+            ];
+        }
+
+        $check_status = ClientStatus::find()->where('id=:id', [':id' => $id])->one();
+
+        if (is_object($check_status)) {
+            Yii::error('Ни передан идентификатор статуса, id:' . serialize($id), __METHOD__);
+
+            return [
+                'status' => 'ERROR',
+                'msg' => 'Данный статус нельзя удалить. Статус используется',
+            ];
+        }
+
+        try {
+            ClientStatus::deleteAll('id=:id', array(':id' => $id));
+        } catch (\Exception $e) {
+            Yii::error('Поймали Exception при удалении статуса: ' . serialize($e->getMessage()), __METHOD__);
+            return false;
+        }
+
+        Yii::info('Статус успешно удален', __METHOD__);
+
+        return [
+            'status' => 'SUCCESS',
+            'msg' => 'Статус успешно удален'
+        ];
+    }
 }
