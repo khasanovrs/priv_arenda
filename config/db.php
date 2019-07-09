bcf65e315160eef83f140955d66fff35a072520c
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-07-09 21:05:54 +0300

    Подключение бд

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=arenda_stroika',
+    /*'dsn' => 'mysql:host=localhost;dbname=arenda_stroika',
     'username' => 'arenda_stroika',
-    'password' => 'arenda_stroika',
+    'password' => 'arenda_stroika',*/
 
-    /*'dsn' => 'mysql:host=localhost;dbname=rdwork_arenda',
+    'dsn' => 'mysql:host=localhost;dbname=rdwork_arenda',
     'username' => 'rdwork_arenda',
-    'password' => 'rdwork_arenda',*/
+    'password' => 'rdwork_arenda',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
