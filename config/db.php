3b4ed199050458bb2a071dec1f12c0eb847c3968
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-07-13 08:39:48 +0300

    Внесение правок

--- config/db.php
+++ config/db.php
@@ -2,14 +2,14 @@
 
 return [
     'class' => 'yii\db\Connection',
-    /*'dsn' => 'mysql:host=localhost;dbname=arenda_stroika',
+    'dsn' => 'mysql:host=localhost;dbname=arenda_stroika',
     'username' => 'arenda_stroika',
-    'password' => 'arenda_stroika',*/
+    'password' => 'arenda_stroika',
 
-    'dsn' => 'mysql:host=localhost;dbname=rdwork_arenda',
+    /*'dsn' => 'mysql:host=localhost;dbname=rdwork_arenda',
     'username' => 'rdwork_arenda',
     'password' => 'rdwork_arenda',
-    'charset' => 'utf8',
+    'charset' => 'utf8',*/
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
