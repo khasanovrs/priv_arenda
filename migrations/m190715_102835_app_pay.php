74671bfe4cfea28655ac28241fcd00376815c794
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-08-17 15:42:24 +0300

    Миграции

--- migrations/m190715_102835_app_pay.php
+++ migrations/m190715_102835_app_pay.php
@@ -14,7 +14,7 @@ class m190715_102835_app_pay extends Migration
     {
         $this->execute("CREATE TABLE `application_pay` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `application_equipment_id` int(11) NOT NULL,
+  `application_equipment_id` int NOT NULL,
   `user_id` int(11) NOT NULL,
   `date_create` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -22,7 +22,7 @@ class m190715_102835_app_pay extends Migration
   KEY `fk_application_pay_2_idx` (`user_id`),
   CONSTRAINT `fk_application_pay_1` FOREIGN KEY (`application_equipment_id`) REFERENCES `application_equipment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_application_pay_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ");
     }
 
