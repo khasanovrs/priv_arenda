658ec47858eeff95621b527f8d7d149ba0c05ca9
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-09-15 14:00:22 +0300

    Внесение правок

---
+++ migrations/m190915_104451_fix_app_eq.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190915_104451_fix_app_eq
+ */
+class m190915_104451_fix_app_eq extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+$this->execute("ALTER TABLE `application_equipment` 
+ADD CONSTRAINT `fk_application_equipment_5`
+  FOREIGN KEY (`hire_state_id`)
+  REFERENCES `hire_state` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190915_104451_fix_app_eq cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190915_104451_fix_app_eq cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
