7a83b7d6d3819dc28ef632e7ae0b380c60c9f902
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-10-08 11:51:57 +0300

    Внесение правок

--- models/EquipmentsCategory.php
+++ models/EquipmentsCategory.php
@@ -12,6 +12,7 @@ use Yii;
  *
  * @property Equipments[] $equipments
  * @property EquipmentsType[] $equipmentsTypes
+ * @property EquipmentsType[] $equipmentsTypes0
  */
 class EquipmentsCategory extends \yii\db\ActiveRecord
 {
@@ -57,6 +58,14 @@ class EquipmentsCategory extends \yii\db\ActiveRecord
      * @return \yii\db\ActiveQuery
      */
     public function getEquipmentsTypes()
+    {
+        return $this->hasMany(EquipmentsType::className(), ['category_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEquipmentsTypes0()
     {
         return $this->hasMany(EquipmentsType::className(), ['category_id' => 'id']);
     }
