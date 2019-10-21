56eb1144a8de1f03c8be66d06eef70cc9da6e8ce
Linaz Rizvanov <rizvan.linaz@gmail.com>
2019-10-21 21:58:03 +0300

    ошибак миграции

--- migrations/m190906_072238_hire_state.php
+++ migrations/m190906_072238_hire_state.php
@@ -15,7 +15,7 @@ class m190906_072238_hire_state extends Migration
         $this->execute("CREATE TABLE `hire_state` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(150) CHARACTER SET 'utf8' NOT NULL,
-  PRIMARY KEY (`id`)) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 ");
     }
 
